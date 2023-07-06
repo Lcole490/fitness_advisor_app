e654a698bf8d55d927ec1bf7239f8f4b152886b7
Lcole.engr <lcole.engr@gmail.com>
2023-07-06 10:57:18 -0400

    initialized src folder with App.js and index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-doom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+import App from './App';
+
+root.render(<App />);
